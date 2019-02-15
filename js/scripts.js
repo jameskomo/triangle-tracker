2893bbd7629b35a51fa514b245e430d7ac40e651
jameskomo <james.komoh@gmail.com>
2019-02-15 17:54:03 +0300

    Loops cerated for all triangles

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+function getValues() {
+  var sideA = parseInt(document.getElementById('sideA').value);
+  var sideB = parseInt(document.getElementById('sideB').value);
+  var sideC = parseInt(document.getElementById('sideC').value);
+  var sides = [sideA, sideB, sideC]
+
+  if ((sideA === sideB) && (sideB === sideC)) {
+    alert("This is an Equilateral Triangle");
+  } else if ((sideA > (sideB + sideC)) || (sideB > (sideA + sideC)) || (sideC > (sideA + sideB))) {
+    alert("This is an NOT a Triangle");
+  } else if ((sideA === sideB) || (sideA === sideC) || (sideB === sideC)) {
+    alert("This is an Isosceles Triangle");
+  } else if (sideA !== sideB && sideB != sideC && sideC != sideA) {
+    // } else if ((sideA !== sideB !== sideC) && (sideA + sideB > sideC) || (sideB + sideC > sideA) || (sideA + sideC > sideB)) {
+    alert("This is an Scalene Triangle");
+  } else {
+    alert("Please enter Traingle lengths");
+  }
+
+}
