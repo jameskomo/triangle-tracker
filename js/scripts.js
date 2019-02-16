7f51f830fd8ded0e48edbca8c9b4505eda3a174c
jameskomo <james.komoh@gmail.com>
2019-02-16 14:48:44 +0300

    Form CSS styling added. Body styling done as well

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
 function isNumberKey(evt){
     var charCode = (evt.which) ? evt.which : event.keyCode
     if (charCode > 31 && (charCode < 48 || charCode > 57))
-        alert("You must enter a number ONLY");
+    return false;
     return true;
 }
 
